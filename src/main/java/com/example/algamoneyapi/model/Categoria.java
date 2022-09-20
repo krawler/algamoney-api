191c308a3660c73f1bfa5fc7c615ae2f41dc269d
rafael <rafael@pop-os.localdomain>
2022-09-19 23:57:39 -0300

    Categoria : Correção migration, criação model, repository e resource

--- src/main/java/com/example/algamoneyapi/model/Categoria.java
+++ src/main/java/com/example/algamoneyapi/model/Categoria.java
@@ -1,5 +1,54 @@
 package com.example.algamoneyapi.model;
 
-public class Categoria {
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return id == other.id && Objects.equals(nome, other.nome);
+	}
+	
 }
