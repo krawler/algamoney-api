1e5507abff78422c733d4261d22600dc1979cdc0
krawler <august.rafael@gmail.com>
2022-11-04 18:30:33 -0300

    Alguns ajustes no lançamento e na classe de tratamento de exceção

--- src/main/java/com/example/algamoneyapi/exceptionhandler/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/example/algamoneyapi/exceptionhandler/AlgaMoneyExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.example.algamoneyapi.exceptionhandler;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.tomcat.util.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -26,6 +30,14 @@ public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemUsuario = message.getMessage("mensagem invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause() != null ? ex.getCause().getMessage().toString() : ex.toString();
 		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+	    String mensagemUsuario = message.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+	    String mensagemDesenvolvedor = ExceptionUtils.unwrapInvocationTargetException(ex).getCause().getMessage();
+	    List<Erro> errors = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+	    return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	public static class Erro {
--- src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -6,15 +6,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.algamoneyapi.event.RecursoCriadoEvent;
 import com.example.algamoneyapi.model.Lancamento;
 import com.example.algamoneyapi.repository.LancamentoRepository;
 import com.example.algamoneyapi.service.LancamentoService;
@@ -28,6 +33,9 @@ public class LancamentoResource {
 	
 	@Autowired
 	private LancamentoRepository repository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
     public ResponseEntity<List<Lancamento>> list(){
@@ -43,7 +51,24 @@ public class LancamentoResource {
 	@PostMapping
 	public ResponseEntity<Lancamento> create(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
 		Lancamento savedLancamento = service.save(lancamento);
-		//publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, savedLancamento.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedLancamento);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Lancamento> update(@Valid @PathVariable Long id, @RequestBody Lancamento lancamento) {
+	    try {
+            Lancamento updatedLancamento = service.update(id, lancamento);
+            return ResponseEntity.ok(updatedLancamento);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("{id}")
+	public ResponseEntity<Lancamento> remove(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
